56a5fecf2a3cbe30b5f5c87e59f3ad5670d47834
Andrew Collier <andrew@fathomdata.dev>
2022-11-01 09:54:15 +0000

    feat: Add r/ and python/ folders; add first R snippet

---
+++ snippets/r/list-to-data-frame.R
@@ -0,0 +1,15 @@
+# Convert a list to a data frame.
+
+capitals <- list(
+    countries = c("England", "Scotland", "Wales"),
+    city = c("London", "Edinburgh", "Cardiff")
+)
+
+# Convert to a data frame.
+#
+as.data.frame(capitals)
+data.frame(capitals)
+
+# Convert to a tibble.
+#
+tibble::as_tibble(capitals)
